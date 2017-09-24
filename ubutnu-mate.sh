1123d58f037643f0dcac819bcf87b64e18225e56
Moki Ding <moki.forum@gmail.com>
2017-09-24 07:38:28 +0200

    Update ubutnu-mate.sh
    Auto confirm action's.

--- ubutnu-mate.sh
+++ ubutnu-mate.sh
@@ -5,26 +5,26 @@ sed -i 's/rs.//g' /etc/apt/sources.list
 apt update
 
 #Basic utilities
-apt install synaptic filezilla vlc git curl unrar leafpad
+apt install synaptic filezilla vlc git curl unrar leafpad -y
 
 #Remove unused things
-apt purge firefox* libreoffice* gnome-orca* deja-dup* atril* rhythmbox* pidgin* thunderbird* plank* pluma* shotwell* synapse* 
-
+apt purge firefox* libreoffice* gnome-orca* deja-dup* atril* rhythmbox* pidgin* thunderbird* plank* pluma* shotwell* synapse* -y
+-y
 #For this core packages better use remove than purge ...
-apt remove ubuntu-mate-welcome mate-utils simple-scan popularity-contest
+apt remove ubuntu-mate-welcome mate-utils simple-scan popularity-contest -y
 
 #Now update system
 apt dist-upgrade
 
 #Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi google-chrome-stable_current_amd64.deb
+gdebi google-chrome-stable_current_amd64.deb -n
 rm -rf gdebi google-chrome-stable_current_amd64.deb
 
 #Install Atom
 wget https://atom.io/download/deb
 mv deb atom.deb
-gdebi atom.deb
+gdebi atom.deb -n
 rm -rf atom.deb
 
 #Install Adapta-Nokto-Eta and icons PPA
@@ -32,4 +32,5 @@ apt-add-repository ppa:tista/adapta -y
 apt-add-repository ppa:noobslab/icons -y
 apt update
 apt install adapta-gtk-theme arc-icons
+
 
